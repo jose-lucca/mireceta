8881dd2ebedbc32841c94d7530c9280aedb1e9f8
jose-lucca <mrjoshejoshe@gmail.com>
2023-11-12 22:42:31 -0300

    agreando_calculadora

---
+++ js/index.js
@@ -0,0 +1,125 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const pantalla = document.querySelector(".pantalla_calc");
+const pantalla2 = document.querySelector(".valor_pantalla");
+const numCero = document.querySelector(".cero_calc");
+const numUno = document.querySelector(".uno_calc");
+const numDos = document.querySelector(".dos_calc");
+const numTres = document.querySelector(".tres_calc");
+const numCuatro = document.querySelector(".cuatro_calc");
+const numCinco = document.querySelector(".cinco_calc");
+const numSeis = document.querySelector(".seis_calc");
+const numSiete = document.querySelector(".siete_calc");
+const numOcho = document.querySelector(".ocho_calc");
+const numNueve = document.querySelector(".nueve_calc");
+const sumarNumero = document.querySelector(".suma_calc");
+
+
+numCero.addEventListener ("click", cero);
+numUno.addEventListener ("click", uno);
+numDos.addEventListener ("click", dos);
+numTres.addEventListener ("click", tres);
+numCuatro.addEventListener ("click", cuatro);
+numCinco.addEventListener ("click", cinco);
+numSeis.addEventListener ("click", seis);
+numSiete.addEventListener ("click", siete);
+numOcho.addEventListener ("click", ocho);
+numNueve.addEventListener ("click", nueve);
+sumarNumero.addEventListener ("click", operadorSuma);
+numResta.addEventListener ("click", restar);
+numMultiplicacion.addEventListener ("click", multiplicar);
+numDivision.addEventListener ("click", dividir);
+numPunto.addEventListener ("click", punto);
+
+
+function cero (){
+    const numeroCero = 0;
+    return pantalla2.value = numeroCero;
+}
+
+function uno (){
+    const numeroUno = 1;
+    return pantalla2.value = numeroUno;
+}
+
+function dos (){
+    const numeroDos = 2;
+    return pantalla2.value = numeroDos;
+}
+
+function tres (){
+    const numeroTres = 3;
+    return pantalla2.value = numeroTres;
+}
+
+
+function cuatro (){
+    const numeroCuatro = 4;
+    return pantalla2.value = numeroCuatro;
+}
+
+function cinco (){
+    const numeroCinco = 5;
+    return pantalla2.value = numeroCinco;
+}
+
+function seis (){
+    const numeroSeis = 6;
+    return pantalla2.value = numeroSeis;
+}
+
+
+function siete (){
+    const numeroSiete = 7;
+    return pantalla2.value = numeroSiete;
+}
+
+function ocho (){
+    const numeroOcho = 8;
+    return pantalla2.value = numeroOcho;
+}
+
+function nueve (){
+    const numeroNueve = 9;
+    return pantalla2.value = numeroNueve;
+}
+
+function operadorSuma (){
+    const op_suma = "+"
+    return pantalla2.value = op_suma;
+}
+
+
+function sumar (){
+    const sum = "+";
+    let numero = []
+    return sum
+}
+
+function restar (){
+    const numeroCero = 0;
+    return pantalla2.value = numeroCero;
+}
+
+function multiplicar (){
+    const numeroCero = 0;
+    return pantalla2.value = numeroCero;
+}
+
+function dividir (){
+    const numeroCero = 0;
+    return pantalla2.value = numeroCero;
+} */
